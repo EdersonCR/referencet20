ea8fec9ab21a06a67e630c41854cf6ad0fdefe93
edersonr <edersonr@comprovei.com>
2022-12-15 21:40:43 -0300

    fix: Remove arquivos .lock

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { theme } from './styles/theme';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
