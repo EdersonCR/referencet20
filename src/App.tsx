781dd927225cb3becce43afa09d7464c6fa8936c
edersonr <edersonr@comprovei.com>
2022-12-15 21:39:57 -0300

    feat: Integração com Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import Section from './components/Section';
 import { spaces } from './styles/theme';
 import { SectionData } from './interfaces/Interfaces';
+import { Analytics } from '@vercel/analytics/react';
 
 const bgImg = require('./images/background.jpg');
 const data: SectionData[] = require('./data/data.json');
@@ -20,11 +21,14 @@ const styles = {
 
 function App() {
   return (
-    <Stack style={styles.background} spacing={`${spaces.standard * 2}rem`}>
-      <Header />
-      {data.map(section => <Section section={section} myKey={`${section.id}`} key={`${section.id}`} />)}
-      <Footer />
-    </Stack>
+    <>
+      <Stack style={styles.background} spacing={`${spaces.standard * 2}rem`}>
+        <Header />
+        {data.map(section => <Section section={section} myKey={`${section.id}`} key={`${section.id}`} />)}
+        <Footer />
+      </Stack>
+      <Analytics />
+    </>
   );
 }
 
