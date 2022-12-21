75e48a4c79817eea6d2dfa20adb0bc54c2100b1d
Éderson Carlos Rodrigues <edersonrodrigues10@hotmail.com>
2022-12-20 22:16:43 -0300

    fix: Erro de digitação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -27,7 +27,7 @@ function Header() {
  return (
   <Box style={headerStyle.body.container}>
     <Typography style={headerStyle.title.text}>
-      Rererência Rápida T20
+      Referência Rápida T20
     </Typography>
     <Typography style={headerStyle.body.text}>
       Este guia apresenta, de maneira resumida e intuitiva, regras básicas de T20 para consulta rápida por jogadores e mestres. As referências das páginas em cada descrição referem-se ao Tormenta20 Livro Básico, edição Jogo do Ano.
